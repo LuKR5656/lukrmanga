fda51f17df60669e3ee262835573d96c685bd660
lukrmanga <lucasr89200@gmail.com>
2025-06-18 18:46:57 +0200

    Maj

---
+++ BP/BP-data.js
@@ -0,0 +1,33 @@
+// BP-data.js
+
+const bpContent = {
+  fr: {
+    title: "🔥 Boiling Point / Oneshot",
+    resume: "Une baston dans un colisée.",
+    chapters: [
+      { name: "Chapitre 1", link: "BP-horizontal.html?chap=chap1" },
+    ]
+  },
+  en: {
+    title: "🔥 Boiling Point / Oneshot",
+    resume: "A fight in a colosseum.",
+    chapters: [
+      { name: "Chapter 1", link: "BP-horizontal.html?chap=chap1" },
+    ]
+  }
+};
+
+const bpPages = {
+  chap1: {
+    fr: [
+      "BP-chap1/fr/page1.jpg",
+      "BP-chap1/fr/page2.jpg",
+      "BP-chap1/fr/page3.jpg"
+    ],
+    en: [
+      "BP-chap1/en/page1.jpg",
+      "BP-chap1/en/page2.jpg",
+      "BP-chap1/en/page3.jpg"
+    ]
+  }
+};
