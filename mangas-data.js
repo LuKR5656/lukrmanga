9a381361913af59f4cb64e7e6e267b0bc5d9a8ac
lukrmanga <lucasr89200@gmail.com>
2025-06-19 17:15:05 +0200

    aaa

--- mangas-data.js
+++ mangas-data.js
@@ -1,18 +1,30 @@
-// mangas-data.js
 const mangas = [
   {
     id: "bp",
     title: "Boiling Point",
     tag: "Oneshot",
-    cover: "covers/bp.jpg",
     chapters: [
       {
-        id: "bp-chap1",
-        title: "BoilingPoint Oneshot",
-        pages: {
-          fr: ["pages/bp/fr/page1.png", "pages/bp/fr/page2.png", "pages/bp/fr/page3.png", "pages/bp/fr/page4.png", "pages/bp/fr/page5.png", "pages/bp/fr/page6.png", "pages/bp/fr/page7.png", "pages/bp/fr/page8.png", "pages/bp/fr/page9.png"],
-          en: ["pages/bp/en/page1.png", "pages/bp/en/page2.png", "pages/bp/en/page3.png", "pages/bp/en/page4.png", "pages/bp/en/page5.png", "pages/bp/en/page6.png", "pages/bp/en/page7.png", "pages/bp/en/page8.png", "pages/bp/en/page9.png"]
-        }
+        number: 1,
+        title: "Unique Chapitre",
+        path: "BP/reader.html"
+      }
+    ]
+  },
+  {
+    id: "eldorado",
+    title: "Eldorado",
+    tag: "Serie",
+    chapters: [
+      {
+        number: 1,
+        title: "Départ",
+        path: "eldorado/chap1/reader.html"
+      },
+      {
+        number: 2,
+        title: "L’île",
+        path: "eldorado/chap2/reader.html"
       }
     ]
   }
