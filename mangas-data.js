cdb828e40a14cd5e5d54de179ab227547372021c
lukrmanga <lucasr89200@gmail.com>
2025-06-19 16:49:43 +0200

    maj

---
+++ mangas-data.js
@@ -0,0 +1,19 @@
+// mangas-data.js
+const mangas = [
+  {
+    id: "bp",
+    title: "Boiling Point",
+    tag: "Oneshot",
+    cover: "covers/bp.jpg",
+    chapters: [
+      {
+        id: "bp-chap1",
+        title: "Chapitre Unique",
+        pages: {
+          fr: ["pages/bp/fr/1.jpg", "pages/bp/fr/2.jpg"],
+          en: ["pages/bp/en/1.jpg", "pages/bp/en/2.jpg"]
+        }
+      }
+    ]
+  }
+];
