108017f96d9ac974c94f4c5f597f27d19f910535
lukrmanga <lucasr89200@gmail.com>
2025-06-19 16:56:38 +0200

    Update mangas-data.js

--- mangas-data.js
+++ mangas-data.js
@@ -8,10 +8,10 @@ const mangas = [
     chapters: [
       {
         id: "bp-chap1",
-        title: "Chapitre Unique",
+        title: "BoilingPoint Oneshot",
         pages: {
-          fr: ["pages/bp/fr/1.jpg", "pages/bp/fr/2.jpg"],
-          en: ["pages/bp/en/1.jpg", "pages/bp/en/2.jpg"]
+          fr: ["pages/bp/fr/page1.png", "pages/bp/fr/page2.png", "pages/bp/fr/page3.png", "pages/bp/fr/page4.png", "pages/bp/fr/page5.png", "pages/bp/fr/page6.png", "pages/bp/fr/page7.png", "pages/bp/fr/page8.png", "pages/bp/fr/page9.png"],
+          en: ["pages/bp/en/page1.png", "pages/bp/en/page2.png", "pages/bp/en/page3.png", "pages/bp/en/page4.png", "pages/bp/en/page5.png", "pages/bp/en/page6.png", "pages/bp/en/page7.png", "pages/bp/en/page8.png", "pages/bp/en/page9.png"]
         }
       }
     ]
