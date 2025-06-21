8abfc713afdbc85326145ee36db1c6fed27ef28d
lukrmanga <lucasr89200@gmail.com>
2025-06-21 22:10:34 +0200

    Update mangas-data.js

--- mangas-data.js
+++ mangas-data.js
@@ -1,73 +1,39 @@
+// Exemple simplifié, mais extensible
 const mangas = [
   {
     id: "bp",
-    title: "Boiling Point",
-    author: "LuK R.",
-    tag: "Oneshot",
-    resume: {
-      fr: "Une baston dans un colisée.",
-      en: "A brawl in a coliseum."
-    },
+    title: "BoilingPoint",
     chapters: [
       {
         id: "bp-1",
         number: 1,
-        title: {
-          fr: "BoilingPoint Oneshot",
-          en: "BoilingPoint Oneshot"
-        },
-        path: "reader.html",
         pages: {
-          fr: "pages/bp/chap1/fr/",
-          en: "pages/bp/chap1/en/"
+          fr: "/page/bp/chap1/fr/",
+          en: "/page/bp/chap1/en/"
         }
       }
-    ],
-    assets: {
-      cover: "assets/cover/BP-cover.png",
-      background: "assets/background/BP-bg.png"
-    }
+    ]
   },
   {
     id: "el",
     title: "Eldorado",
-    author: "LuK R.",
-    tag: "Serie",
-    resume: {
-      fr: "Une aventure vers une île légendaire pleine de mystères.",
-      en: "An adventure to a legendary island full of secrets."
-    },
     chapters: [
       {
         id: "el-1",
         number: 1,
-        title: {
-          fr: "Le Nouveau Monde",
-          en: "the New World"
-        },
-        path: "reader.html",
         pages: {
-          fr: "pages/el/chap1/fr/",
-          en: "pages/el/chap1/en/"
+          fr: "/page/el/chap1/fr/",
+          en: "/page/el/chap1/en/"
         }
       },
-      {
-        id: "el-2",
-        number: 2,
-        title: {
-          fr: "L’île",
-          en: "The Island"
-        },
-        path: "reader.html",
-        pages: {
-          fr: "pages/el/chap2/fr/",
-          en: "pages/el/chap2/en/"
+    {
+       id: "el-2",
+       number: 2,
+       pages: {
+         fr: "/page/el/chap2/fr/",
+         en: "/page/el/chap2/en/"
         }
       }
-    ],
-    assets: {
-      cover: "assets/cover/EL-cover.png",
-      background: "assets/background/EL-bg.png"
-    }
+    ] 
   }
 ];
