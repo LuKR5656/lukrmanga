c3c213bf1be568c195869900cb65883e77424937
lukrmanga <lucasr89200@gmail.com>
2025-06-21 23:31:28 +0200

    Update mangas-data.js

--- mangas-data.js
+++ mangas-data.js
@@ -7,7 +7,7 @@ const mangas = [
       {
         id: "bp-1",
         number: 1,
-        numPages: 9,   
+        numPages: 10,
         pages: {
           fr: "/pages/bp/chap1/fr/",
           en: "/pages/bp/chap1/en/"
@@ -18,74 +18,37 @@ const mangas = [
   {
     id: "el",
     title: "Eldorado",
+    author: "LuK R.",
     chapters: [
       {
         id: "el-1",
         number: 1,
-        numPages: 11,
+        numPages: 14,
         pages: {
           fr: "/pages/el/chap1/fr/",
           en: "/pages/el/chap1/en/"
-        }
-      },
-      {
-        id: "el-2",
-        number: 2,
-        pages: {
-          fr: "/pages/el/chap2/fr/",
-          en: "/pages/el/chap2/en/"
         }
       }
     ]
   }
 ];
 
-
-
-
-
 /*
-//const mangas = [
-  // Série existante ou nouvel ajout :
-  {
-    id: "id_unique_de_la_serie",         // exemple : "bp", "el"
-    title: "TitreDeLaSerie",             // exemple : "BoilingPoint"
-        author: "LuK R.",                // exemple : "LuK R."
-    chapters: [
-      {
-        id: "id_unique_chapitre",        // exemple : "bp-1"
-        number: 1,                       // numéro du chapitre
-        numPages: 1,                     // nombre de pages
-        pages: {
-          fr: "/pages/serie/chap1/fr/",   // lien vers la version FR
-          en: "/pages/serie/chap1/en/"    // lien vers la version EN
-          // ajoute d'autres langues si besoin
-        }
-      },
-      // Copier/coller ce bloc pour chaque chapitre supplémentaire :
-      // {
-      //   id: "id_unique_chapitre2",
-      //   number: 2,
-      //   pages: {
-      //     fr: "/page/serie/chap2/fr/",
-      //     en: "/page/serie/chap2/en/"
-      //   }
-      // }
-    ]
-  },
-  // Copier/coller ce bloc pour chaque nouvelle série :
-  // {
-  //   id: "nouvelle_serie",
-  //   title: "TitreNouvelleSerie",
-  //   chapters: [
-  //     {
-  //       id: "nouvelle_serie-1",
-  //       number: 1,
-  //       pages: {
-  //         fr: "/page/nouvelle_serie/chap1/fr/",
-  //         en: "/page/nouvelle_serie/chap1/en/"
-  //       }
-  //     }
-  //   ]
-  // }
-];
+EXEMPLE POUR AJOUTER UNE SÉRIE :
+{
+  id: "nouvelle_serie",
+  title: "TitreNouvelleSerie",
+  author: "Auteur...",
+  chapters: [
+    {
+      id: "nouvelle_serie-1",
+      number: 1,
+      numPages: 12,
+      pages: {
+        fr: "/pages/nouvelle_serie/chap1/fr/",
+        en: "/pages/nouvelle_serie/chap1/en/"
+      }
+    }
+  ]
+}
+*/
