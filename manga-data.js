8ffd4d7d02c087ea24d0d1ea6ebc25e202c7961f
lukrmanga <lucasr89200@gmail.com>
2025-07-08 17:02:17 +0200

    Update manga-data.js

--- manga-data.js
+++ manga-data.js
@@ -39,7 +39,7 @@ const mangas = [
           en: "pages/el/chap1/en/"
         },
         numPages: 11 // le nombre réel d'images 
-      },
+      }/*,
       {
         id: "el-2",
         title: { fr: "Chapitre 2", en: "Chapter 2" },
